f5ed4286c21093813f7cc7de96e89f24d55e3aa2
Anton Krutoy <2191@mail.ru>
2024-07-02 22:27:03 +0500

    02-07-2024 2227

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,19 +10,18 @@ data class Post(
     val can_edit: Boolean,
     val is_pinned: Boolean
 )
-//) {
-//    var likes = likes
-//        set(value) {
-//            if (value >= 0) {
-//                field = value
-//            }
-//        }
-//}
 
-object Wallservice {
 
-    private var posts = emptyArray<Post>()
+object WallService {
+
+    //private var posts = emptyArray<Post>()
+    private val posts = ArrayList<Post>()
+    private var nextId = 1
+
     fun add(post: Post): Post {
+        val newPost = post.copy(id = nextId++)
+        posts.add(newPost)
+        return newPost
         posts += post
         return posts.last()
     }
@@ -35,19 +34,55 @@ object Wallservice {
         }
     }
 
+    fun update(post: Post): Boolean {
+        val index = posts.indexOfFirst { it.id == post.id }
+        return if (index != -1) {
+            posts[index] = post
+            true
+        } else {
+            false
+        }
+    }
+
+    fun getPosts(): ArrayList<Post> {
+        return posts
+    }
+
+
 }
 
 fun main() {
+    //создали НезмПермеренную post и передали в data Post данные для поста
+    //который уже создал запись данных
     val post = Post(
         1, 12, 54, 20, "record test test", 5,
         true, false, true, true
     )
+    WallService.add(post)
 
-    //post.likes=7
-    //println(post.id)
-    //println(post.likes)
+    val post2 = Post(
+        2, 312, 254, 120, "record2222 test test", 15,
+        true, false, true, true
+    )
+    WallService.add(post2)
 
+    //создали НеизПеременную liked и скопировали в нее "запись данных post" созданную ранее
+    //только изменив поле likes добавил +3
     val liked = post.copy(likes = post.likes + 3)
     println(liked)
 
+
+// Получение списка постов
+    val posts = WallService.getPosts()
+    println("Wall posts: $posts")
+
+
+    // Обновляем пост с id = 1
+    val updatedPost = Post(1, 12, 54, 20, "NEW NEW record1 test test", 5,
+        true, false, true, true)
+    val isUpdated = WallService.update(updatedPost)
+
+
+    val postsNew = WallService.getPosts()
+    println("Wall posts: $postsNew")
 }
