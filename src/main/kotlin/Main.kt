cd90ea4a067a6fb3129788ce36ec648ce4ae9b6a
Anton Krutoy <2191@mail.ru>
2024-07-03 16:52:46 +0500

    03072024 1652

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-data class Answer(val id:Int,val date:Int)
-data class Messege(val id:Int,val date:Int)
+data class answer(val id: Int, val date: Int)
+data class messege(val id: Int, val date: Int)
 
-data class Post(
+data class post(
     val id: Int,
     val owner_id: Int,
     val from_id: Int,
@@ -12,19 +12,19 @@ data class Post(
     val can_delete: Boolean,
     val can_edit: Boolean,
     val is_pinned: Boolean,
-    val Messege:Messege?=null,
-    val Answer: Answer? =null
+    val messege: answer? = null,
+    val answer: answer? = null
 )
 
 
-object WallService {
+object wallService {
 
     //private var posts = emptyArray<Post>()
-    private val posts = ArrayList<Post>()
+    private val posts = ArrayList<post>()
     private var nextId = 1
 
-    fun add(post: Post): Post {
-        val newPost = post.copy(id = nextId++)
+    fun add(post: post): post {
+        val newPost = post.copy(id = nextId++, answer = post.answer?.copy(), messege = post.messege?.copy())
         posts += newPost
         return posts.last()
     }
@@ -32,22 +32,22 @@ object WallService {
     fun likedById(id: Int) {
         for ((index, post) in posts.withIndex()) {
             if (post.id == id) {
-                posts[index] = post.copy(likes = post.likes + 1)
+                posts[index] = post.copy(likes = post.likes + 1,answer = post.answer?.copy(), messege = post.messege?.copy() )
             }
         }
     }
 
-    fun update(post: Post): Boolean {
+    fun update(post: post): Boolean {
         val index = posts.indexOfFirst { it.id == post.id }
         return if (index != -1) {
-            posts[index] = post
+            posts[index] = post.copy()
             true
         } else {
             false
         }
     }
 
-    fun getPosts(): ArrayList<Post> {
+    fun getPosts(): ArrayList<post> {
         return posts
     }
 
@@ -57,17 +57,17 @@ object WallService {
 fun main() {
     //создали НезмПермеренную post и передали в data Post данные для поста
     //который уже создал запись данных
-    val post = Post(
+    val post = post(
         1, 12, 54, 20, "record test test", 5,
         true, false, true, true
     )
-    WallService.add(post)
+    wallService.add(post)
 
-    val post2 = Post(
+    val post2 = post(
         2, 312, 254, 120, "record2222 test test", 15,
         true, false, true, true
     )
-    WallService.add(post2)
+    wallService.add(post2)
 
     //создали НеизПеременную liked и скопировали в нее "запись данных post" созданную ранее
     //только изменив поле likes добавил +3
@@ -76,16 +76,18 @@ fun main() {
 
 
 // Получение списка постов
-    val posts = WallService.getPosts()
+    val posts = wallService.getPosts()
     println("Wall posts: $posts")
 
 
     // Обновляем пост с id = 1
-    val updatedPost = Post(1, 12, 54, 20, "NEW NEW record1 test test", 5,
-        true, false, true, true)
-    val isUpdated = WallService.update(updatedPost)
+    val updatedPost = post(
+        1, 12, 54, 20, "NEW NEW record1 test test", 5,
+        true, false, true, true
+    )
+    val isUpdated = wallService.update(updatedPost)
 
 
-    val postsNew = WallService.getPosts()
+    val postsNew = wallService.getPosts()
     println("Wall posts: $postsNew")
 }
